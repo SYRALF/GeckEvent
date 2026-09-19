27cff8f504a044cc3625832023dd1914868ffcac
Chinomasacre <junioryque2014@gmail.com>
2026-09-19 10:37:20 -0500

    bases del backend + configuracion de docker + configuracion de la bd

---
+++ Backend/api/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+
+  await app.listen(port);
+
+  console.log(
+    `Servidor corriendo en http://localhost:${port}`,
+  );
+}
+
+bootstrap();
